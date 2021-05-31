64b7262ff1adde03275deee4deee03141a8b80a9
AaronWangTW <68408616+AaronWangTW@users.noreply.github.com>
2021-06-01 01:00:46 +0800

    fixed app from func to class

--- frontwebpage/src/App.js
+++ frontwebpage/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import {
   Navbar,
   NavbarBrand,
@@ -10,28 +10,30 @@ import {
 
 import Plot from 'react-plotly.js';
 
-function App() {
-  return (
-    <div>
-      <Navbar color="dark" light expand="md" className="spaceLeft">
-        <NavbarBrand href="/" className="text-white">Mingdao iGEM Bacterial Phage Simulation Visualizer</NavbarBrand>
-        <NavbarText className="floatRight text-white">Mingdao iGEM 2021</NavbarText>
-      </Navbar>
-      <br></br><Button color="secondary" className="marginLeft">Start Simulation</Button><br></br><br></br>
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-        ]}
-        layout={ {autosize: true, title: 'Bacteria Population Size'} }
-      />
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" light expand="md" className="spaceLeft">
+          <NavbarBrand href="/" className="text-white">Mingdao iGEM Bacterial Phage Simulation Visualizer</NavbarBrand>
+          <NavbarText className="floatRight text-white">Mingdao iGEM 2021</NavbarText>
+        </Navbar>
+        <br></br><Button color="secondary" className="marginLeft">Start Simulation</Button><br></br><br></br>
+        <Plot
+          data={[
+            {
+              x: [1, 2, 3],
+              y: [2, 6, 3],
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: { color: 'red' },
+            },
+          ]}
+          layout={{ autosize: true, title: 'Bacteria Population Size' }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
